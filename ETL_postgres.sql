615bdd59e5145094488f0e34102c1ff3a6ce4401
chaseroldan <61606603+chaseroldan@users.noreply.github.com>
2020-06-11 22:01:36 -0700

    postgres

--- ETL_postgres.sql
+++ ETL_postgres.sql
@@ -29,3 +29,9 @@ SELECT dow_open from dow
 SELECT * from dow
 
 SELECT * from eth
+
+CREATE TABLE dow_eth AS
+SELECT dow.date, dow.dow_open, dow.dow_close, eth.eth_open, eth.eth_close
+FROM dow
+JOIN eth
+ON dow.date = eth.date;;
